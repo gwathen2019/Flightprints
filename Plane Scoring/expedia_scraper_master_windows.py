b47980045faf3f56586288ba3780eb1a3ed39543
Rachel-Chloe Gregory <rachel.chloe.gregory@gmail.com>
2020-02-01 11:35:40 -0600

    fixed for heroku

--- Plane Scoring/expedia_scraper_master_windows.py
+++ Plane Scoring/expedia_scraper_master_windows.py
@@ -6,8 +6,16 @@ def scrape():
     import numpy as np
     import requests
 
+    GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
+    CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
+
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.binary_location = GOOGLE_CHROME_PATH
+
     try:
-        browser = webdriver.Chrome('c:/Users/chromedriver.exe')
+        browser = webdriver.Chrome(execution_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
 
         date = '03/27/2020'
         start = 'BNA'
