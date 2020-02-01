591c1b81af015606a2e458444813af0b833e47da
Rachel-Chloe Gregory <rachel.chloe.gregory@gmail.com>
2020-02-01 10:17:29 -0600

    Merging files

--- Plane Scoring/expedia_scraper_airplane_model.py
+++ Plane Scoring/expedia_scraper_airplane_model.py
@@ -23,19 +23,56 @@ def scrape():
     # model = list_element.text                   
     # print(model)
     model = []
+    airline = []
+    depart = []
+    arrive = []
+    duration = []
+    no_stops = []
+    depart_arrival = []
+    price = []
+    layover = []
+    depart_apt = []
+    arrive_apt = []
+    layover_apt = []
 
     for i in range(1,6):
         dropdown_element = browser.find_elements_by_xpath('/html/body/div[2]/div[8]/section/div/div[10]/ul/li['+str(i)+']/div[1]/div[2]/span[1]/a/span[4]')[0]
         browser.execute_script("arguments[0].click();", dropdown_element)
-        list_element = browser.find_elements_by_xpath('/html/body/div[2]/div[8]/section/div/div[10]/ul/li['+str(i)+']/div[1]/div[3]/div[2]/div/section/div/div[1]/ul/li[5]')[0]
-        model.append(list_element.text)      
-        print(i)          
-        print(model)
+        model_element = browser.find_elements_by_xpath('/html/body/div[2]/div[8]/section/div/div[10]/ul/li['+str(i)+']/div[1]/div[3]/div[2]/div/section/div/div[1]/ul/li[5]')[0]
+        model.append(model_element.text) 
+        airline_id_element = browser.find_elements_by_xpath('/html/body/div[2]/div[8]/section/div/div[10]/ul/li['+str(i)+']/div[1]/div[1]/div[1]/div/div/div/div[1]/div[2]/span')[0]
+        airline.append(airline_id_element.text)
+        depart_time_element = browser.find_elements_by_xpath('/html/body/div[2]/div[8]/section/div/div[10]/ul/li['+str(i)+']/div[1]/div[1]/div[1]/div/div/div/div[1]/div[1]/span/span[1]')[0]
+        depart.append(depart_time_element.text)
+        arrive_time_element = browser.find_elements_by_xpath('/html/body/div[2]/div[8]/section/div/div[10]/ul/li['+str(i)+']/div[1]/div[1]/div[1]/div/div/div/div[1]/div[1]/span/span[4]')[0]
+        arrive.append(arrive_time_element.text)
+        duration_element = browser.find_elements_by_xpath('/html/body/div[2]/div[8]/section/div/div[10]/ul/li['+str(i)+']/div[1]/div[1]/div[1]/div/div/div/div[2]/div[1]/span[1]')[0]
+        duration.append(duration_element.text)
+        no_stops_element = browser.find_elements_by_xpath('/html/body/div[2]/div[8]/section/div/div[10]/ul/li['+str(i)+']/div[1]/div[1]/div[1]/div/div/div/div[2]/div[1]/span[2]')[0]
+        no_stops.append(no_stops_element.text)
+        depart_arrival_apt_element = browser.find_elements_by_xpath('/html/body/div[2]/div[8]/section/div/div[10]/ul/li['+str(i)+']/div[1]/div[1]/div[1]/div/div/div/div[2]/div[2]')[0]
+        depart_arrival.append(depart_arrival_apt_element.text)
+        price_element = browser.find_elements_by_xpath('/html/body/div[2]/div[8]/section/div/div[10]/ul/li['+str(i)+']/div[1]/div[1]/div[2]/div/div[1]/div[1]/span')[0]
+        price.append(price_element.text)
 
     for i in range(0,len(model)):
         item = model[i].split(" |")
         model[i] = item[0]
     
+    for i in range(0,len(depart_arrival)):
+        item = depart_arrival[i].split("\n")
+        if len(item) > 4:
+            layover.append(True)
+            depart_apt.append(item[1].split(' ')[0])
+            layover_apt.append(item[2].split(' ')[-1])
+            arrive_apt.append(item[-1].split(' ')[-1])
+        else:
+            layover.append(False)
+            depart_apt.append(item[1].split(' ')[0])
+            arrive_apt.append(item[-1].split(' ')[-1])
+            
+        print(layover)
+
     print(model)
 
     # for image in images:
@@ -62,7 +99,7 @@ def scrape():
     # results_dict['mars_weather']=mars_weather
     # results_dict['html_table']=html_table
     # results_dict['hemisphere_image_urls']=hemisphere_image_urls
-    # browser.quit()
+    browser.quit()
     # return(results_dict)
 
 scrape()
